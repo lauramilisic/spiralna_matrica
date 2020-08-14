c069c3205fe1022bda390a2b6217117f9d9b4c24
Laura Milišić <laura.milisic@outlook.com>
2020-08-14 12:31:32 +0200

    drugi unos

--- src/zadatak/SpiralnaMatrica.java
+++ src/zadatak/SpiralnaMatrica.java
@@ -1,62 +1,76 @@
 package zadatak;
 
-public class SpiralnaMatrica {
-	
-		static int MAX = 100; 
+import java.util.Scanner;
 
-	
-		static void spiralnaMatrica(int m, int n, int a[][]) { 
-			
-			int val = 1; 
-
-			int k = 0, l = 0; 
-			while (k < m && l < n) { 
-				
-				for (int i = l; i < n; ++i) { 
-					a[k][i] = val++; 
-				} 
-
-				k++; 
-
-				
-				for (int i = k; i < m; ++i) { 
-					a[i][n - 1] = val++; 
-				} 
-				n--; 
-
-				
-				if (k < m) { 
-					for (int i = n - 1; i >= l; --i) { 
-						a[m - 1][i] = val++; 
-					} 
-					m--; 
-				} 
-
-				
-				if (l < n) { 
-					for (int i = m - 1; i >= k; --i) { 
-						a[i][l] = val++; 
-					} 
-					l++; 
-				} 
-			} 
-		}
-
-		
-		public static void main(String[] args) { 
-			int m = 10, n = 10; 
-			int a[][] = new int[MAX][MAX]; 
-			spiralnaMatrica(m, n, a); 
-			for (int i = 0; i < m; i++) { 
-				for (int j = 0; j < n; j++) { 
-					System.out.print(a[i][j] + " "); 
-				} 
-				System.out.println(""); 
-			} 
-		} 
+public class SpiralnaMatrica  
+{
+	 public static void main(String args[])
+	    {
+	        System.out.println("Enter The Value For N :");
+	         
+	        Scanner sc = new Scanner(System.in);
+	         
+	        int n = sc.nextInt();
+	         
+	        int[][] spiral = new int[n][n];
+	         
+	        int value = 1;
+	         
+	        int minCol = 0;
+	         
+	        int maxCol = n-1;
+	         
+	        int minRow = 0;
+	         
+	        int maxRow = n-1;
+	         
+	        while (value <= n*n)
+	        {
+	            for (int i = minCol; i <= maxCol; i++)
+	            {
+	                spiral[minRow][i] = value;
+	                     
+	                value++;
+	            }
+	             
+	            for (int i = minRow+1; i <= maxRow; i++) 
+	            { 
+	                spiral[i][maxCol] = value; 
+	                 
+	                value++; 
+	            } 
+	             
+	            for (int i = maxCol-1; i >= minCol; i--)
+	            {
+	                spiral[maxRow][i] = value;
+	                             
+	                value++;
+	            }
+	             
+	            for (int i = maxRow-1; i >= minRow+1; i--) 
+	            {
+	                spiral[i][minCol] = value;
+	                 
+	                value++;
+	            }
+	             
+	            minCol++;
+	             
+	            minRow++;
+	             
+	            maxCol--;
+	             
+	            maxRow--;
+	        }
+	         
+	        for (int i = 0; i < spiral.length; i++)
+	        {
+	            for (int j = 0; j < spiral.length; j++)
+	            {
+	                System.out.print(spiral[i][j]+ "\t");
+	            }
+	             
+	            System.out.println();
+	        }
+	    }
 	}
-
-	
-
-
-
